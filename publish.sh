4fd019f1d3f670ff9275efb44e74a985991d848d
Doc MO <maueroats@gmail.com>
2017-10-09 21:54:37 -0500

    wy-cs site update 2017-10-09 21:54

--- publish.sh
+++ publish.sh
@@ -4,7 +4,8 @@ if [ -z "$base_dir" ]; then echo "$0: must set base_dir!" 1>&2 ; exit 1 ; fi
 
 cd "$base_dir"
 echo "should delete data?" 
-echo rm -r docs
+echo "yes..."
+rm -rf docs
 hugo
 git add -A
 git commit -a -m "wy-cs site update $(date +'%Y-%m-%d %H:%M')"
