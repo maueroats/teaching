d3fdea166c149a2634571d873be42293f08ca75f
Doc Mo <maueroats@gmail.com>
2019-01-29 15:24:17 -0600

    wy-cs site update 2019-01-29 15:24

--- docs/ap-cs/jm/ch12/12-quiz-p/12-quiz-p.java
+++ docs/ap-cs/jm/ch12/12-quiz-p/12-quiz-p.java
@@ -14,8 +14,9 @@ Examples:
 8. `thumb2019([50,15,5])` => `[50,28,5]`
 1. `thumb2019([10,30,70,100])` => `[10,40,65,100]`
 2. `thumb2019([40,47,54,61])` => `[40,47,54,61]`
-3. `thumb2019([40,47,55,2019,65])` => `[40,48,55,60,65]`
-4. `thumb2019([1,1,3,4,2019,5,2019])` => `[1,1,3,4,2019,5,2019]`
+3. `thumb2019([40,47,55,2019,65])` => `[40,47,55,60,65]`
+4. `thumb2019([1,1,3,4,2019,5,2019])` => `[1,1,3,4,5,5,2019]`
+5. `thumb2019([36,47,55,2019,65]`) => `[36,46,55,60,65]`
 */
 
 // omitted "public" class for repl.it
@@ -26,63 +27,67 @@ class Main {
 
     /* This is the method you write: */
     public static int[] thumb2019 (int[] nums) {
-	return nums;
+        return nums;
     }
 
     public static int testnum = 0 ;
     public static void test1(int[] nums, int correct[]) {
-	int[] orig = Arrays.copy(nums);
-	int[] answer = thumb2019(nums);
-	String result = "FAIL";
-	boolean ok = false;
-	if ( Arrays.equals(answer,correct) ) {
-	    result = "OK";
-	    ok = true;
-	}
-	if ( Arrays.equals(orig, nums) ) {
-	    result = "NO CHANGES MADE";
-	}
+        int[] orig = Arrays.copyOf(nums, nums.length);
+        int[] answer = thumb2019(nums);
+        String result = "FAIL";
+        boolean ok = false;
+        if ( Arrays.equals(answer,correct) ) {
+            result = "OK";
+            ok = true;
+        }
+        if ( Arrays.equals(orig, nums) ) {
+            result += " NO CHANGES MADE";
+        }
 
-	testnum += 1;
-	System.out.println("Test #"+testnum+": "+result);
+        testnum += 1;
+        System.out.println("Test #"+testnum+": "+result);
 
-	if ( ! ok ) {
-	    System.out.println("thumb2019("+Arrays.toString(orig)") = "+Arrays.toString(answer)+")");
-	}
+        if ( ! ok ) {
+            System.out.println("thumb2019("+Arrays.toString(orig)+") = "+Arrays.toString(answer)+")");
+        }
     }
     public static void main (String[] args)
     {
-	int[] t1 = {5,15,31};
-	int[] a1 = {5,18,30};
-	test(t1,a1);
+        int[] t1 = {5,15,31};
+        int[] a1 = {5,18,31};
+        test1(t1,a1);
 
-	int[] t2 = {5,15,2019};
-	int[] a2 = {5,15,2019};
-	test1(t2,a2);
+        int[] t2 = {5,15,2019};
+        int[] a2 = {5,15,2019};
+        test1(t2,a2);
 
-	int[] t3 = {51,15,5};
-	int[] a3 = {51,28,5};
-	test1(t3,a3);
+        int[] t3 = {51,15,5};
+        int[] a3 = {51,28,5};
+        test1(t3,a3);
 
-	int[] t4 = {50,15,5};
-	int[] a4 = {50,28,5};
-	test1(t4,a4);
+        int[] t4 = {50,15,5};
+        int[] a4 = {50,28,5};
+        test1(t4,a4);
 
-	
-	int[] t5 = {10,30,70,100};
-	int[] a5 = {10,40,65,100};
-	test1(t5,a5);
+        
+        int[] t5 = {10,30,70,110};
+        int[] a5 = {10,40,75,110}; // 75 or 65 ok
+        test1(t5,a5);
 
-	int[] t6 = {40,47,54,61};
-	int[] a6 = {40,47,54,61};
-	test1(t6,a6);
-	
-	int[] t7 = {40,47,55,2019,65};
-	int[] a7 = {40,48,55,60,65};
-	test1(t7,a7);
-	
-	int[] t8 = {1,1,3,4,2019,5,2019};
-	int[] a8 = {1,1,3,4,2019,5,2019};
-	test1(t8,a8);
+        int[] t6 = {40,47,54,61};
+        int[] a6 = {40,47,54,61};
+        test1(t6,a6);
+        
+        int[] t7 = {40,47,55,2019,65};
+        int[] a7 = {40,47,55,60,65};
+        test1(t7,a7);
+        
+        int[] t8 = {1,1,3,4,2019,5,2019};
+        int[] a8 = {1,1,3,4,5,5,2019};
+        test1(t8,a8);
+
+        int[] t9 = {36,47,55,2019,65};
+        int[] a9 = {36,46,55,60,65};
+        test1(t9,a9);
     }
 }
