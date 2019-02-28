879c7b5c269baa1fb93c35ab6706bc0ae749d2f6
Andrew Mauer-Oats <MauerOats@gmail.com>
2019-02-27 20:53:16 -0600

    wy-cs site update 2019-02-27 20:53

--- docs/ap-cs/jm/ch11/11-quiz-b/TestClothes.java
+++ docs/ap-cs/jm/ch11/11-quiz-b/TestClothes.java
@@ -1,6 +1,16 @@
 
 public class TestClothes {
 
+    // on repl.it the assert() method does nothing
+    public static void aassert(bool b)
+    {
+	if (b) {
+	    System.out.println("OK");
+	} else {
+	    System.out.println("BAD");
+	}
+    }
+
     public static void main(String[] args) {
         Clothes hat = new HeadCovering(20, 105);
         Clothes underArmor = new WoolClothes (hat);
@@ -11,13 +21,13 @@ public class TestClothes {
         w.add(denim);
 
         Person p1 = new PlainPerson("Jane");
-        assert(50 == p1.getFashion());
-        assert(90 == p1.getWarmth());
-        assert("Jane".equals(p1.getName()));
+        aassert(50 == p1.getFashion());
+        aassert(90 == p1.getWarmth());
+        aassert("Jane".equals(p1.getName()));
 
         Person p2 = new BigFashionPerson("Sanjana", w);
-        assert("Sanjana".equals(p2.getName()));
-        assert(20+25+95 == p2.getWarmth());
-        assert(40 == p2.getFashion());
+        aassert("Sanjana".equals(p2.getName()));
+        aassert(20+25+95 == p2.getWarmth());
+        aassert(40 == p2.getFashion());
     }
 }
